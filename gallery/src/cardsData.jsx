6d61c6dd055c967dcd783ec1dc76d051500605a0
Tiaguh <tiagosingup@gmail.com>
2024-04-15 07:52:41 -0300

    finishing imports

--- gallery/src/cardsData.jsx
+++ gallery/src/cardsData.jsx
@@ -1,5 +1,19 @@
 import Starry_Night from './Assets/Starry_Night.jpg';
 import The_Storm_on_the_Lake_of_Galilee from './Assets/The_Storm_on_the_Lake_of_Galilee.jpg';
+import Lady_with_an_Ermine from './Assets/Lady_with_an_Ermine.jpg';
+import Le_Garçon_au_gilet_rouge from './Assets/Le_Garçon_au_gilet_rouge.jpg';
+import Girl_with_a_Pearl_Earring from './Assets/Girl_with_a_Pearl_Earring.jpg';
+import The_Great_Wave_off_Kanagawa from './Assets/The_Great_Wave_off_Kanagawa.jpg';
+import The_Night_Cafe from './Assets/The_Night_Cafe.jpg';
+import The_Arnolfini_portrait from './Assets/The_Arnolfini_portrait.jpg';
+import Guernica from './Assets/Guernica.jpg';
+import Van_Gogh_Self_Portrait from './Assets/Vincent_van_Gogh_-_Self-Portrait.jpg';
+import Monalisa from './Assets/Mona_Lisa,_by_Leonardo_da_Vinci.jpg';
+import Maria_Maddalena_Penitente from './Assets/Maria_Maddalena_Penitente.jpg';
+import The_Sleeping_Gypsy from './Assets/The Sleeping Gypsy.jpg';
+import The_Basket_of_Apples from './Assets/The_Basket_of_Apples.jpg';
+import The_Swing from './Assets/The_Swing.jpg'
+
 
 const cardsData = [
   {
@@ -14,7 +28,99 @@ const cardsData = [
     "painting": The_Storm_on_the_Lake_of_Galilee,
     "painting_name": "The Storm on the Sea of Galilee",
     "artist_name": "Rembrandt"
-  }
+  },
+
+  {
+    "id": 3,
+    "painting": Lady_with_an_Ermine,
+    "painting_name": "Lady with an Ermine",
+    "artist_name": "Leonardo da Vinci"
+  },
+
+  {
+    "id": 4,
+    "painting": Le_Garçon_au_gilet_rouge,
+    "painting_name": "Le Garçon au gilet rouge",
+    "artist_name": "Paul Cézanne"
+  },
+
+  {
+    "id": 5,
+    "painting": Girl_with_a_Pearl_Earring,
+    "painting_name": "Girl with a earl Earring",
+    "artist_name": "Johannes Vermeer"
+  },
+
+  {
+    "id": 6,
+    "painting": The_Great_Wave_off_Kanagawa,
+    "painting_name": "The Great Wave off Kanagawa",
+    "artist_name": "Katsushika Hokusai"
+  },
+
+  {
+    "id": 7,
+    "painting": The_Night_Cafe,
+    "painting_name": "The Night Café",
+    "artist_name": "Van Gogh"
+  },
+
+  {
+    "id": 8,
+    "painting": The_Arnolfini_portrait,
+    "painting_name": "The Arnolfini portrait",
+    "artist_name": ""
+  },
+
+  {
+    "id": 9,
+    "painting": Guernica,
+    "painting_name": "Guernica",
+    "artist_name": "Pablo Picasso"
+  },
+
+  {
+    "id": 10,
+    "painting": Van_Gogh_Self_Portrait,
+    "painting_name": "Van Gogh Self Portrait",
+    "artist_name": "Vincent Van Gogh"
+  },
+
+  {
+    "id": 11,
+    "painting": Monalisa,
+    "painting_name": "Mona Lisa",
+    "artist_name": "Leonardo da Vinci"
+  },
+
+  {
+    "id": 12,
+    "painting": Maria_Maddalena_Penitente,
+    "painting_name": "Maria Maddalena Penitente",
+    "artist_name": "Artemisia Gentileschi"
+  },
+
+  {
+    "id": 13,
+    "painting": The_Sleeping_Gypsy,
+    "painting_name": "The Sleeping Gypsy",
+    "artist_name": "Henri Rousseau"
+  },
+
+  {
+    "id": 14,
+    "painting": The_Basket_of_Apples,
+    "painting_name": "The Basket of Apples",
+    "artist_name": "Paul Cézanne"
+  },
+
+  {
+    "id": 15,
+    "painting": The_Swing,
+    "painting_name": "The Swing",
+    "artist_name": "Jean-Honoré Fragonard"
+  },
+
 ];
 
 export default cardsData;

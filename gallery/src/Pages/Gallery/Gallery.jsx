943573c7c470b16429f29012664f7135294bedd0
Tiaguh <tiagosingup@gmail.com>
2024-04-20 20:54:57 -0300

    creating props

--- gallery/src/Pages/Gallery/Gallery.jsx
+++ gallery/src/Pages/Gallery/Gallery.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Header from '../../components/Header/Header';
 
-export default function Gallery() {
+export default function Gallery(props) {
   return (
-    <div></div>
+    <div key={props.id} >
+      <Header />
+
+      <div>
+        <img src={props.img} />
+        <h1>{props.painting_name}</h1>
+        <p>{props.artist_name}</p>
+      </div>
+
+      <div>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias magni porro eos odit modi rerum, numquam suscipit. Animi, fuga, nulla nisi cumque placeat mollitia earum officiis modi adipisci exercitationem vitae.o</p>
+      </div>
+
+    </div>
   )
 }

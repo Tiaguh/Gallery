36f49c579f9662744fd906a227177547bed777d3
Tiaguh <tiagosingup@gmail.com>
2024-04-22 13:56:39 -0300

    adjusting the interface

--- gallery/src/Pages/Gallery/Gallery.jsx
+++ gallery/src/Pages/Gallery/Gallery.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import "./Gallery.css"
+
 import { useParams } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 
@@ -13,15 +15,32 @@ export default function Gallery() {
     <div>
       <Header />
 
-      {painting ? (
-        <div>
-          <img src={painting.painting} alt={painting.painting_name} />
-          <h1>{painting.painting_name}</h1>
-          <p>{painting.artist_name}</p>
-        </div>
-      ) : (
-        <p>Pintura não encontrada.</p>
-      )}
+      <div className="gallery-container">
+
+        {painting ? (
+          <div className="gallery">
+
+            <div className="gallery-infos">
+
+              <div className="gallery-img">
+                <img src={painting.painting} alt={painting.painting_name} />
+              </div>
+
+              <div className="gallery-info">
+                <h1>{painting.painting_name}</h1>
+                <p>{painting.artist_name}</p>
+              </div>
+            </div>
+
+            <div className="gallery-description">
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quod praesentium laborum vitae ipsa rem architecto voluptate in numquam. Unde, dicta quas illo velit perspiciatis ipsum magni expedita quo consequatur!</p>
+            </div>
+
+          </div>
+        ) : (
+          <p>Pintura não encontrada.</p>
+        )}
+      </div>
     </div>
   )
 }
